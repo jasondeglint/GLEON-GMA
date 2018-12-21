d0bb911a85e3f4d9dc8fb4a3ccee192dd1452207
Sam Feng <samfeng279@gmail.com>
2018-12-21 04:47:05 -0500

    Removal of redundant module imports

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import pandas as pd
 import numpy as np
 import data_analysis as da
 from settings import df, months, cols, year, years, locations, USEPA_LIMIT, WHO_LIMIT 
--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,3 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
 import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
